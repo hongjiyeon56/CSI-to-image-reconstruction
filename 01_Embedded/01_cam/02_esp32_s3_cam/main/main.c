b5c5abb9eadd8158cf9e76ac08cc26fbc35b93dd
ChanyeongJu <chanyeong.ju@ipinlabs.com>
2026-02-02 14:22:10 +0900

    fix(cam): load nvs configuration before wifi initialization to apply dynamic settings

--- 01_Embedded/01_cam/02_esp32_s3_cam/main/main.c
+++ 01_Embedded/01_cam/02_esp32_s3_cam/main/main.c
@@ -235,9 +235,8 @@ void app_main(void) {
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-    wifi_init_sta();
-
     nvs_load_config();
+    wifi_init_sta();
 
     if (ESP_OK != init_camera()) {
         return;
